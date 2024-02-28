38ae9c5b059f0974ca60d54f71f719d9e5202b74
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-02-29 01:19:07 +0900

    フッター作成

--- inc/assets.php
+++ inc/assets.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 
-function ws_register_style () {
-    wp_enqueue_style('ws-header-style', get_template_directory_uri(). '/assets/css/header.css');
+function ws_register_style()
+{
+    wp_enqueue_style('ws-header-style', get_template_directory_uri() . '/assets/css/header.css');
+    wp_enqueue_style('ws-footer-style', get_template_directory_uri() . '/assets/css/footer.css');
 }
 
 add_action('wp_enqueue_scripts', 'ws_register_style');
