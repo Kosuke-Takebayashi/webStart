47422d7ddb85a27b4613a7b6f8b1090b7ddf663d
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-03-08 22:58:00 +0900

    PCとタブレットのコンテンツ幅設定メニューを作成幅設定メニューを作成

--- inc/customizer.php
+++ inc/customizer.php
@@ -66,13 +66,13 @@ function webstart_customize_preview_js()
 add_action('customize_preview_init', 'webstart_customize_preview_js');
 
 /**
- * メニューを追加
+ * コンテンツ幅のメニューを追加
  */
 function my_customizer_menu($wp_customize)
 {
 
 	$wp_customize->add_panel(
-		'my_panel',
+		'pc_panel',
 		array(
 			'title'    => 'コンテンツ幅',
 			'priority' => 1,
@@ -80,13 +80,31 @@ function my_customizer_menu($wp_customize)
 	);
 
 	$wp_customize->add_section(
-		'my_section',
+		'pc_section',
 		array(
 			'title'    => 'PC',
-			'panel'    => 'my_panel',
+			'panel'    => 'pc_panel',
 			'priority' => 1,
 		)
 	);
+
+	$wp_customize->add_section(
+		'tb_section',
+		array(
+			'title'    => 'タブレット',
+			'panel'    => 'pc_panel',
+			'priority' => 2,
+		)
+	);
+
+	// $wp_customize->add_section(
+	// 	'sp_section',
+	// 	array(
+	// 		'title'    => 'スマートフォン',
+	// 		'panel'    => 'sp_panel',
+	// 		'priority' => 3,
+	// 	)
+	// );
 
 	$wp_customize->add_setting('my_contentWidth');
 	$wp_customize->add_control(
@@ -95,12 +113,39 @@ function my_customizer_menu($wp_customize)
 			'my_control',
 			array(
 				'label'    => '最大幅（px）',
-				'section'  => 'my_section',
+				'section'  => 'pc_section',
 				'settings' => 'my_contentWidth',
 				'priority' => 1,
 			)
 		)
 	);
+
+	$wp_customize->add_setting('my_contentWidthTb');
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'my_control_tb',
+			array(
+				'label'    => '最大幅（px）',
+				'section'  => 'tb_section',
+				'settings' => 'my_contentWidthTb',
+				'priority' => 2,
+			)
+		)
+	);
+
+	// $wp_customize->add_control(
+	// 	new WP_Customize_Control(
+	// 		$wp_customize,
+	// 		'my_control',
+	// 		array(
+	// 			'label'    => '最大幅（px）',
+	// 			'section'  => 'sp_section',
+	// 			'settings' => 'my_contentWidthSp',
+	// 			'priority' => 1,
+	// 		)
+	// 	)
+	// );
 }
 add_action('customize_register', 'my_customizer_menu');
 
@@ -108,11 +153,25 @@ function webstart_add_contentWidth_css()
 {
 	// 設定した最大幅を取得
 	$contentWidth = get_theme_mod('my_contentWidth');
+	$contentWidthTb = get_theme_mod('my_contentWidthTb');
+	// $contentWidthSp = get_theme_mod('my_contentWidthSp');
 ?>
 	<style type="text/css">
 		.content-width {
 			max-width: <?php echo $contentWidth; ?>px;
 		}
+
+		@media screen and (max-width: 767px) {
+			.content-width {
+				max-width: <?php echo $contentWidthTb; ?>px;
+			}
+		}
+
+		/* @media screen and (max-width: 599px) {
+			.content-width {
+				max-width: <?php echo $contentWidthSp; ?>px;
+			}
+		} */
 	</style>
 <?php
 }
