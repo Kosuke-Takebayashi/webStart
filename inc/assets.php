95695f57b94fdfa84e6e7cff8879091ca80a3646
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-03-08 21:37:51 +0900

    h2、h3見出しのスタイルを追加

--- inc/assets.php
+++ inc/assets.php
@@ -1,10 +1,11 @@
 <?php
 
-function ws_register_style()
+function webstart_register_style()
 {
-    wp_enqueue_style('ws-header-style', get_template_directory_uri() . '/assets/css/header.css');
-    wp_enqueue_style('ws-main-style', get_template_directory_uri() . '/assets/css/main.css');
-    wp_enqueue_style('ws-footer-style', get_template_directory_uri() . '/assets/css/footer.css');
+    wp_enqueue_style('webstart-header-style', get_template_directory_uri() . '/assets/css/header.css');
+    wp_enqueue_style('webstart-main-style', get_template_directory_uri() . '/assets/css/main.css');
+    wp_enqueue_style('webstart-footer-style', get_template_directory_uri() . '/assets/css/footer.css');
+    wp_enqueue_style('webstart-custom-style', get_template_directory_uri() . '/assets/css/custom.css');
 }
 
-add_action('wp_enqueue_scripts', 'ws_register_style');
+add_action('wp_enqueue_scripts', 'webstart_register_style');
